9b2101ffaeab7b696f24aa6402894d22197870bf
justinmvail <justinmvail@gmail.com>
2017-05-11 22:17:37 -0400

    adding guidbox mirror api functionality and exposing more api endpoints

--- src/main/java/com/glean/controller/MovieController.java
+++ src/main/java/com/glean/controller/MovieController.java
@@ -4,10 +4,12 @@ import com.glean.guideBoxDataEntities.Movie;
 import com.glean.guideBoxAccessLayer.GuideBoxAPIAccessor;
 import com.glean.guideBoxAccessLayer.GuideBoxDataAggregator;
 import com.glean.repository.MovieRepo;
+import com.glean.wrappers.MovieListWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -29,21 +31,37 @@ public class MovieController {
     @Value("${apikey}")
     String apiKey;
 
-    @RequestMapping("movie/{id}")
+    @RequestMapping(value = "movie/{id}", method = RequestMethod.GET)
     public Movie getShow(@PathVariable(value="id") String id) {
         return movieRepo.findById(id);
     }
 
-    @RequestMapping("movie")
+    @RequestMapping(value = "movie", method = RequestMethod.GET)
     public List<Movie> getMovieByTitle(@RequestParam(value="title") String title) {
         List<Movie> movies = movieRepo.findByTitle(title);
         return movies;
     }
 
-//    @RequestMapping(value = "guidebox/movie", method = RequestMethod.GET)
-//    public List<Movie> getMovieFromGuideBoxByTitle(@RequestParam(value="title") String title) {
-//
-//
-//    }
+    @RequestMapping(value = "guidebox/movie", method = RequestMethod.GET)
+    public MovieListWrapper getMovieFromGuideBoxByTitle(@RequestParam(value="title") String title){
+        MovieListWrapper movieListWrapper = null;
+        try {
+            movieListWrapper = dataAggregator.fetchAndAssembleMovieListFromGuideBoxByTitle(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return movieListWrapper;
+    }
+
+    @RequestMapping(value = "guidebox/movie", method = RequestMethod.POST)
+    public void addMovieToGleanFromGuideBox(@RequestParam(value="id") String id){
+        try {
+            movieRepo.save(dataAggregator.fetchAndAssembleMovieFromGuideBox(id));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
